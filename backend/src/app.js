d4438caff92d462c9b0c81a0562b67139bc6bf94
Avranil Dhar <179360557+AvranilDhar@users.noreply.github.com>
2025-10-01 06:16:14 +0530

    add clerl

--- backend/src/app.js
+++ backend/src/app.js
@@ -1,5 +1,6 @@
 import express from "express";
 import cookieparser from "cookie-parser";
+import { clerkMiddleware } from '@clerk/express'
 
 
 const app = express();
@@ -7,12 +8,15 @@ const app = express();
 app.use(express.json({
     limit : "10kb",
 }));
+
 app.use(express.urlencoded({
     extended : true,
     limit : "10kb"
 }));
+
 app.use(cookieparser());
 
+app.use(clerkMiddleware());
 
 
 export { app };
